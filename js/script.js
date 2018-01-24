cef593a7d24b9ee2a8ade7dadbae7e5a954cf770
Shan W <shan.w@outlook.com>
2018-01-24 17:36:55 +0400

    Displays snippet, along with URL. Added "enter to search" functionality for search button.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
-//(function(){
-//  var newAJAX = new XMLHttpRequest();
-//  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=butterfly&format=json&origin=*"
-//
-//  newAJAX.open('GET', url);
-//  newAJAX.send();
-//  newAJAX.responseType = 'json';
-//
-//  newAJAX.onload = function(){
-//    results = newAJAX.response;
-//    console.log(results);
-//  }
-//})();
-
-document.getElementById("sbtn").onclick = box;
 let resDisp = document.getElementById("container");
+let searchBox = document.getElementById("searchBox");
+let searchBtn = document.getElementById("sbtn");
 
-function box(){
-  let search = document.getElementById("searchBox").value;
-  let hasChild = resDisp.querySelector("#container ul") != null;
-  if(hasChild){
-    let ul = document.querySelector("#container ul");
-    ul.parentNode.removeChild(ul);
+// Pressing "ENTER" or clicking button - initiates API call
+searchBtn.onclick = sbox;
+searchBox.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13){
+      searchBtn.click();
+    }
+  })
+
+// Stores user search text, removes previous search items, if any.
+function sbox(){
+  let searchText = document.getElementById("searchBox").value;
+  let errorMsg = document.getElementById("error");
+
+  if (searchText !== ""){
+    let hasChild = resDisp.querySelector("#container ul") != null;
+
+    if(hasChild){
+      let ul = document.querySelector("#container ul");
+      ul.parentNode.removeChild(ul);
+    }
+
+    wikiSearch(searchText);
   }
-  wikiSearch(search);
 }
 
+// Initiates API call with search text
 function wikiSearch(search){
   let newAJAX = new XMLHttpRequest();
-  let url = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*"
+  let page = 0;
+  let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=info&list=search&generator=search&inprop=url&srlimit=10&sroffset=1&srprop=snippet&gsrlimit=10&gsroffset=1"
 
-  newAJAX.open('GET', url+"&search="+search);
+  newAJAX.open('GET', url+"&srsearch="+search+"&gsrsearch="+search);
   newAJAX.send();
   newAJAX.responseType = 'json';
 
@@ -38,30 +43,48 @@ function wikiSearch(search){
 //    console.log(results);
     displayResults(results);
   }
-
 }
 
+// Displays the results, title and snippet, along with URL to page
 function displayResults(results){
-  let resArr = [];
+  let snippets = results.query.search;
+  let urls = results.query.pages;
+  let urlArr = [];
+
   let ul = document.createElement('ul');
 
-  for(let i=0; i<(results[1].length>10 ? 10 : results[1].length); i++){
+  console.log(snippets);
+  console.log(urls);
+
+  for (let key in urls){
+    urlArr.push(urls[key].fullurl);
+  }
+
+  console.log(urlArr);
+  console.log(snippets[0].title);
+
+  for(let i=0; i<(urlArr.length>10 ? 10 : urlArr.length); i++){
     let a = document.createElement('a');
     let li = document.createElement('li');
-    a.href = results[3][i];
-    a.text = results[1][i];
+    let p = document.createElement('p');
+
+    a.href = urlArr[i];
+    a.text = snippets[i].title + ": ";
     a.setAttribute("target", "_blank");
+
+    p.innerHTML = snippets[i].snippet;
+
     li.appendChild(a);
+    li.appendChild(p);
     ul.appendChild(li);
     resDisp.appendChild(ul);
   }
-
-
-
 //  alert(resArr[1]);
 }
 
 
+// TODO: Find a way to get user language, and change API endpoint to that language
+// FUTURE: Change to jQuery
 
 
 
